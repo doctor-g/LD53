22f4ad0b595384cea1ef26867a2d1072672fa8ba
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-04-29 15:23:44 -0400

    Configure for web export

--- project/test_level.gd
+++ project/test_level.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+## This cannot be based on the "viewport" since in HTML5, it might be a weird size.
+const _CAMERA_MIN_X := 350 # 700/2
 
 var _tween : Tween
 var _previous_pos : Vector2
@@ -9,16 +11,11 @@ var _dropped := false
 ## Effectively a constant but calculated from the physics settings
 var _pixels_per_meter :float = ProjectSettings.get_setting("physics/2d/default_gravity") / 9.8
 
-## Effectively a constant, computed in ready.
-var _camera_min_x : int
-
 @onready var _man :Node2D = $Man
 @onready var _package :RigidBody2D = $Package
 @onready var _camera := $Camera2D
 
 func _ready():
-	_camera_min_x = get_viewport().size.x / 2
-	
 	_tween = create_tween()
 	_tween.tween_property(_package, "position", _package.position+Vector2(30,0), 1.0).set_trans(Tween.TRANS_CUBIC)
 	_tween.tween_property(_package, "position", _package.position-Vector2(30,0), 1.0).set_trans(Tween.TRANS_CUBIC)	
@@ -30,7 +27,7 @@ func _ready():
 
 func _physics_process(delta:float):
 	if _dropped:
-		_camera.global_position.x = max(_camera_min_x, _package.global_position.x)
+		_camera.global_position.x = max(_CAMERA_MIN_X, _package.global_position.x)
 	
 	if Input.is_action_just_pressed("drop"):
 		_dropped = true
