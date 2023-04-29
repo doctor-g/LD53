1190c263c316c350ba46bc174f953e6c4c7ed6fa
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-04-29 11:20:08 -0400

    Print distance traveled

--- project/test_level.gd
+++ project/test_level.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
+
 var _tween : Tween
 var _previous_pos : Vector2
 
+## Effectively a constant but calculated from the physics settings
+var _pixels_per_meter :float = ProjectSettings.get_setting("physics/2d/default_gravity") / 9.8
+
+@onready var _man :Node2D = $Man
 @onready var _package :RigidBody2D = $Package
 
 func _ready():
@@ -12,15 +17,28 @@ func _ready():
 	_tween.set_ease(Tween.EASE_IN_OUT)
 	_tween.set_loops()
 
+
 func _physics_process(delta:float):
 	if Input.is_action_just_pressed("drop"):
 		_tween.stop()
-		$Package.freeze = false
+		_package.freeze = false
 		
 		# Apply impulse in the direction the package is moving from the tween.
 		var speed := _package.global_position - _previous_pos
-		var momentum := speed * ($Package.mass as float) / delta
-		$Package.apply_impulse(momentum)
+		var momentum := speed * (_package.mass as float) / delta
+		_package.apply_impulse(momentum)
+		
+		# Watch for the package to come to rest
+		_package.sleeping_state_changed.connect(_on_package_sleeping_state_changed)
 
 	# Track position so that momentum can be computed
 	_previous_pos = _package.global_position
+
+
+func _on_package_sleeping_state_changed()->void:
+	if _package.sleeping:
+		var pixel_distance := _package.global_position.x - _man.global_position.x
+		var meters := pixel_distance / _pixels_per_meter
+		print("DISTANCE IS ", meters, "m")
+		
+		_package.sleeping_state_changed.disconnect(_on_package_sleeping_state_changed)
