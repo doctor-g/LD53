f978f9cf2e5ae31d47e0f5efa57f90ec26b876ee
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-04-29 19:39:58 -0400

    Enhance title screen
    Though I cannot remember exactly how.

--- project/ui/title_screen.gd
+++ project/ui/title_screen.gd
@@ -1,5 +1,11 @@
 extends Control
 
+var _advanced := false
+
+func _ready():
+	if OS.get_name()!="Web":
+		$Camera2D.offset.y += 700
+
 
 func _on_play_button_button_down():
 	%ClickSound.play()
@@ -9,3 +15,9 @@ func _on_play_button_button_up():
 	%ClickSound.play()
 	await %ClickSound.finished
 	get_tree().change_scene_to_file("res://test_level.tscn")
+
+
+func _input(event):
+	if not _advanced and event is InputEventMouseButton and event.is_pressed():
+		_advanced=true
+		$AnimationPlayer.play("move_camera_down")
