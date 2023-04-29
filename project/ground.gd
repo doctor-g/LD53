de0836ea8b5d5626fa937770def41893367cf96b
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-04-29 16:22:24 -0400

    Fix ground appearance

--- project/ground.gd
+++
@@ -1,7 +0,0 @@
-extends StaticBody2D
-
-@onready var _shape :RectangleShape2D = $CollisionShape2D.shape
-
-func _draw():
-	var rect := Rect2(-_shape.size.x/2, -_shape.size.y/2, _shape.size.x, _shape.size.y)
-	draw_rect(rect, Color.DARK_GREEN)
