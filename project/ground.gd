073f568574d811234d7f127d687d6f8490f3bec9
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-04-29 08:34:57 -0400

    Add a ground

---
+++ project/ground.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+
+@onready var _shape :RectangleShape2D = $CollisionShape2D.shape
+
+func _draw():
+	var rect := Rect2(-_shape.size.x/2, -_shape.size.y/2, _shape.size.x, _shape.size.y)
+	draw_rect(rect, Color.DARK_GREEN)
+	print("Drawn at ", rect)
