b314d464eb6c65c1f6069b0d97ebf3911a5e1cf3
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-04-29 20:25:15 -0400

    Add an audible button abstraction
    It makes clicky sounds.

---
+++ project/ui/audible_button.gd
@@ -0,0 +1,10 @@
+extends Button
+
+
+func _on_button_down():
+	Sfx.play_click_sound()
+
+
+func _on_button_up():
+	Sfx.play_click_sound()
+	
--- project/ui/title_screen.gd
+++ project/ui/title_screen.gd
@@ -7,17 +7,12 @@ func _ready():
 		$Music.play()
 
 
-func _on_play_button_button_down():
-	Sfx.play_click_sound()
-
-
-func _on_play_button_button_up():
-	Sfx.play_click_sound()
-	get_tree().change_scene_to_file("res://test_level.tscn")
-
-
 func _input(event):
 	if Globals.show_splash and event is InputEventMouseButton and event.is_pressed():
 		Globals.show_splash = false
 		$AnimationPlayer.play("move_camera_down")
 		$Music.play()
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://test_level.tscn")
